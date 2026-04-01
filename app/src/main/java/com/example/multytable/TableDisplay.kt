3e8e80efc220360f094879ec0770165b55549f95
sipokobridget-sketch <sipokobridget@gmail.com>
2026-04-01 16:54:49 +0200

    data on table display page

--- app/src/main/java/com/example/multytable/TableDisplay.kt
+++ app/src/main/java/com/example/multytable/TableDisplay.kt
@@ -1,6 +1,7 @@
 package com.example.multytable
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,17 @@ class TableDisplay : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_table_display)
+
+        //assigning data from my main page to variables
+        val bundle: Bundle? = intent.extras
+        //getting the number entered by user as a string
+        val tableString: String? = bundle?.getString("tableNumber")
+        //converting the string to an integer
+        val tableNumber = tableString?.toInt()
+        val multiplyTable = findViewById<TextView>(R.id.tableDisplaytxt)
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
