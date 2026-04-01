34ace75d9092158564649de142291ce4cc103653
sipokobridget-sketch <sipokobridget@gmail.com>
2026-04-01 16:27:27 +0200

    added comments

--- app/src/main/java/com/example/multytable/MainActivity.kt
+++ app/src/main/java/com/example/multytable/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.multytable
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,8 +16,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val multiplication = findViewById<Button>(R.id.multplyBtn)
+        val numInput = findViewById<EditText>(R.id.numTextInput)
+
+        //created a button click listener to listen for the button click
         multiplication.setOnClickListener {
-            //create intent
+            /*Created an intent to switch between pages(switching from main activity to table display activity)*/
             val intent = Intent(this, TableDisplay::class.java)
             //start activity
             startActivity(intent)
